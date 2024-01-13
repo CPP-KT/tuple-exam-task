b8ca890157cff730314eb82f721977780c5f1ca9
Artem Koton <kotonartem@protonmail.com>
2024-01-13 22:02:48 +0300

    Update task

--- src/tuple.h
+++ src/tuple.h
@@ -1,65 +1,55 @@
 #pragma once
 
-// #include <tuple>
-// using namespace std;
+#include <cstddef>
 
 template <typename... Types>
-struct tuple {
-  // same types constructors
-  // note: think *when* you need to enable all of them
+class tuple {
+public:
   constexpr tuple();
+
   constexpr tuple(const Types&... args);
 
-  tuple(const tuple& other);
-  tuple(tuple&& other);
-
-  // no need for conditional explicitness
-  // convert types constructors
   template <typename... UTypes>
   constexpr tuple(UTypes&&... args);
 
   template <typename... UTypes>
   constexpr tuple(const tuple<UTypes...>& other);
+
   template <typename... UTypes>
   constexpr tuple(tuple<UTypes...>&& other);
 };
 
-// make_tuple -- constructor with auto deducing types
-// note references are dereferenced
-// see cppreference
+// deduction guide to aid CTAD
+template <typename... Types>
+tuple(Types...) -> tuple<Types...>;
+
 template <typename... Types>
 constexpr tuple<VTypes...> make_tuple(Types&&... args);
 
-// NOTE: if not tuple passed to helpers -- must be compiler error
-// tuple_element -- return type by it's number (type field)
-template <std::size_t N, typename... Types>
-struct tuple_element;
-
-// pls implement tuple_element_t
-
-// tuple_size -- value equals number of elements (you can use std::integral_constant)
-template <typename... Types>
+template <typename T>
 struct tuple_size;
 
-// pls implement tuple_size_v
+template <typename T>
+inline constexpr std::size_t tuple_size_v = 0;
 
-// get -- main function to interact with tuple
-// NOTE: think how to replace int in return type
-// NOTE: compiler error if N > tuple_size
-template <std::size_t N, typename... Types>
-constexpr int& get(tuple<Types...>& t) noexcept;
+template <std::size_t N, typename T>
+struct tuple_element;
 
-template <std::size_t N, typename... Types>
-constexpr int&& get(tuple<Types...>&& t) noexcept;
+template <std::size_t N, typename T>
+using tuple_element_t = void;
 
 template <std::size_t N, typename... Types>
-constexpr const int& get(const tuple<Types...>& t) noexcept;
+constexpr Type& get(tuple<Types...>& t) noexcept;
 
 template <std::size_t N, typename... Types>
-constexpr const int&& get(const tuple<Types...>&& t) noexcept;
+constexpr Type&& get(tuple<Types...>&& t) noexcept;
 
-// gets by type
-// NOTE: compiler error if many T in one tuple
+template <std::size_t N, typename... Types>
+constexpr const Type& get(const tuple<Types...>& t) noexcept;
+
+template <std::size_t N, typename... Types>
+constexpr const Type&& get(const tuple<Types...>&& t) noexcept;
+
 template <typename T, typename... Types>
 constexpr T& get(tuple<Types...>& t) noexcept;
 
@@ -72,20 +62,8 @@ constexpr const T& get(const tuple<Types...>& t) noexcept;
 template <typename T, typename... Types>
 constexpr const T&& get(const tuple<Types...>&& t) noexcept;
 
-// swap specialization
 template <typename... TTypes, typename... UTypes>
-void swap(tuple<TTypes...>& first, tuple<UTypes...>& second);
+constexpr bool operator==(const tuple<TTypes...>& lhs, const tuple<UTypes...>& rhs);
 
-// compare operators
 template <typename... TTypes, typename... UTypes>
-constexpr bool operator==(const tuple<TTypes...>& first, const tuple<UTypes...>& second);
-template <typename... TTypes, typename... UTypes>
-constexpr bool operator!=(const tuple<TTypes...>& first, const tuple<UTypes...>& second);
-template <typename... TTypes, typename... UTypes>
-constexpr bool operator<(const tuple<TTypes...>& first, const tuple<UTypes...>& second);
-template <typename... TTypes, typename... UTypes>
-constexpr bool operator>(const tuple<TTypes...>& first, const tuple<UTypes...>& second);
-template <typename... TTypes, typename... UTypes>
-constexpr bool operator<=(const tuple<TTypes...>& first, const tuple<UTypes...>& second);
-template <typename... TTypes, typename... UTypes>
-constexpr bool operator>=(const tuple<TTypes...>& first, const tuple<UTypes...>& second);
+constexpr void operator<=>(const tuple<TTypes...>& lhs, const tuple<UTypes...>& rhs);
